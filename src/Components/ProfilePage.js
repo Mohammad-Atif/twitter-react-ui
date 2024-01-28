8ad0db59f2b906179639f0d527db0ef573fc1613
Akash <a3328307@gmail.com>
2024-01-28 22:14:07 +0530

    login and logged out ui updated

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -1,35 +1,67 @@
 import { useState } from "react";
 import { ProfileButton } from "./Profile/ProfileButton";
-import {AllProfiles} from "./Profile/AllProfiles"
+import { AllProfiles } from "./Profile/AllProfiles";
 import { MyProfile } from "./Profile/MyProfile";
+import { Navigate } from "react-router-dom";
 
 export const ProfilePage = () => {
-    const [profileClicked,setProfileClicked] = useState(false)
-    const [myProfileClicked,setMyProfileClicked] = useState(false)
-    const onProfileClick = () => {
-        setProfileClicked(true);
-    }
-    const onMyProfileClick = () => {
-        setMyProfileClicked(true);
-    }
-    const handleCloseClick = () => {
-        setProfileClicked(()=> {
-            return false;
-        })
-        
-    }
+  const [profileClicked, setProfileClicked] = useState(false);
+  const [myProfileClicked, setMyProfileClicked] = useState(false);
+  const [logout, setLogout] = useState(false);
+  const [logoutText, setLogoutText] = useState(
+    "You Have Successfully Logged Out!!"
+  );
+  const [displayText, setDisplayText] = useState(false);
+  const onProfileClick = () => {
+    setDisplayText(false);
+    setProfileClicked(true);
+  };
+  const onMyProfileClick = () => {
+    setDisplayText(false);
+    setMyProfileClicked(true);
+  };
+  const handleCloseClick = () => {
+    setDisplayText(false);
+    setProfileClicked(() => {
+      return false;
+    });
+  };
 
-    const handleMyProfileCloseClick = () => {
-        setMyProfileClicked(()=> {
-            return false;
-        })
-    }
+  const handleMyProfileCloseClick = () => {
+    setDisplayText(false);
+    setMyProfileClicked(() => {
+      return false;
+    });
+  };
 
-    return (
-        <div className="profilePage">
-            {myProfileClicked == true?<MyProfile onCloseClick={handleMyProfileCloseClick}/>:<ProfileButton onClickBtn={onMyProfileClick} text= {"My Profile"}/>}
-            {profileClicked == true ? <AllProfiles onCloseClick={handleCloseClick}/>:<ProfileButton onClickBtn={onProfileClick} text= {"All profile"}/>}
-            <ProfileButton onClickBtn={() => {}} text= {"Log Out"}/>
-        </div>
-    );
-}
+  const logutOutHandler = () => {
+    setDisplayText(true);
+    setLogout(true);
+    setLogoutText("You Have Successfully Logged Out!!");
+  };
+  const logInHandler = () => {
+    setLogout(false);
+    setLogoutText("You Have Successfully Logged In");
+  };
+
+  return (
+    <div className="profilePage">
+      <div>{displayText && logoutText}</div>
+      {myProfileClicked == true ? (
+        <MyProfile onCloseClick={handleMyProfileCloseClick} />
+      ) : (
+        <ProfileButton onClickBtn={onMyProfileClick} text={"My Profile"} />
+      )}
+      {profileClicked == true ? (
+        <AllProfiles onCloseClick={handleCloseClick} />
+      ) : (
+        <ProfileButton onClickBtn={onProfileClick} text={"All profile"} />
+      )}
+      {!logout ? (
+        <ProfileButton onClickBtn={logutOutHandler} text={"Log Out"} />
+      ) : (
+        <ProfileButton onClickBtn={logInHandler} text={"Log In"} />
+      )}
+    </div>
+  );
+};
